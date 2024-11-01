03005c959ac239c05b6e0242e41c31b5326bbef1
void436 <skander436bh@gmail.com>
2024-11-01 12:42:03 -0400

    Adding the c++ file

---
+++ team2_finalProject_code_exercise2.cpp
@@ -0,0 +1,139 @@
+/*
+
+READ user's choice from main menu 
+COMPLY with user's choice 
+FOR 1: DISPLAY a single product info
+FOR 2: DISPLAY all product info
+FOR 3: CALCULATE the user's purchase with taxes and total 
+       DISPLAY the receit 
+FOR 4:  CALCULATE and DISPLAY stock  quantity and be able to add  to stock
+FOR 5: EXIT
+
+*/     
+
+#include <iostream>
+using namespace std;
+
+
+//declaring functions
+
+void write_productInfo (int code);
+int read_mainMenu ();
+int read_stockMenu();
+void stock_inventory (int code);
+void calculate_User_Purchase ();
+void calculate_Add_ToStock ( int code, int quantity );
+
+
+//Main Program
+int main (){
+
+    //Declare variables and constants and initialize
+
+
+
+    //Invite, read and save inputs
+
+
+
+    //Calculate
+
+
+
+    //Display outputs
+
+
+
+
+    return 0;
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//robert
+
+void write_productInfo (int code){
+
+}
+
+int read_mainMenu () {
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//fin
+
+
+int read_stockMenu(){
+
+}
+ 
+void stock_inventory (int code) {
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//skander
+
+
+
+void calculate_User_Purchase (){
+
+}
+ 
+void calculate_Add_ToStock ( int code, int quantity ){
+
+}
+
+
