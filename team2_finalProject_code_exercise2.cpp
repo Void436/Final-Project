c9f35e2141fd499919f18a44051173d7378ba2ad
Void <skander436bh@gmail.com>
2024-11-26 11:25:46 -0500

    Adding comments to the code

--- team2_finalProject_code_exercise2.cpp
+++ team2_finalProject_code_exercise2.cpp
@@ -47,6 +47,7 @@ void show_Receipt(const Product products[], int size, const int purchased_qty[])
 
 int main() {
 
+    //Declare variables and constants and initialize
     Product small = {50, "Small", 15.99, 33};
     Product medium = {100, "Medium", 24.99, 14};
     Product big = {250, "Big", 49.99, 17};
@@ -56,43 +57,61 @@ int main() {
     int user_choice;
     int purchased_qty[4] = {0, 0, 0, 0};
 
+
     do {
 
+        //Invite, read and save inputs
         user_choice = read_MainMenu();
 
 
         if (user_choice == 1) {
+            
+            //Display outputs
             write_Stock_Inventory(products, 4);
+
         } 
 
         
         else if (user_choice == 2) {
 
             cout << endl;
+
             for (int i = 0; i < 4; i++) {
+
+                //Display outputs
                 information(products[i]);
+            
             }
         } 
 
         
         else if (user_choice == 3) {
+            
+            //Calculate
             calculate_User_Purchase(products, 4, purchased_qty);
+
         } 
 
         
         else if (user_choice == 4) {
 
+            //Declare variables and constants and initialize
             int stock_choice = 0;
 
             do {
+                
+                //Invite, read and save inputs
                 stock_choice = read_StockMenu();
 
 
                 if (stock_choice == 1) {
                     
+                    //Declare variables and constants and initialize
                     int code;
                     bool code_verif = false;
 
+                    
+                    //Invite, read and save inputs
                     do {
 
                         cout << "Enter the CODE of the product (50, 100, 250, or 500) to display its details: " << endl;
@@ -119,24 +138,28 @@ int main() {
 
                     if (code == 50) {
                         
+                        //Display outputs
                         one_Information(products[0]);
                     
                     }
 
                     else if (code == 100) {
                         
+                        //Display outputs
                         one_Information(products[1]);
                     
                     }
 
                     else if (code == 250) {
                         
+                        //Display outputs
                         one_Information(products[2]);
                     
                     }
 
                     else if (code == 500) {
                         
+                        //Display outputs
                         one_Information(products[3]);
                     
                     }
@@ -145,6 +168,7 @@ int main() {
                 
                 else if (stock_choice == 2) {
 
+                    //Calculate
                     replenish_Stock_Inventory(products, 4);
 
                 }
@@ -158,6 +182,8 @@ int main() {
         
     } while (user_choice != 5);
 
+
+    //Display outputs
     cout << endl << "Thank you for using CALCULATOR PLUS! See you next time!" << endl << endl;
 
     return 0;
@@ -334,16 +360,19 @@ int read_MainMenu() {
 
 void show_Receipt(const Product products[], int size, const int purchased_qty[]) {
  
+    //Declare variables and constants and initialize
     float total_before_tax = 0;
     float total_tps = 0;
     float total_tvq = 0;
     float grand_total = 0;
     int total_products = 0;
  
+    //Display outputs
     cout << fixed << setprecision(2);
     cout << endl << "----------------------------------------------------------------------------------------------------" << endl;
     cout << "Code\tDimension\tQty\tUnit Price\tSubtotal\tTPS\tTVQ\tTotal" << endl;
  
+    //Calculate
     for (int i = 0; i < size; i++) {
         if (purchased_qty[i] > 0) {
             float subtotal = purchased_qty[i] * products[i].unit_price;
@@ -357,12 +386,14 @@ void show_Receipt(const Product products[], int size, const int purchased_qty[])
             grand_total += total;
             total_products += purchased_qty[i];
  
+            //Display outputs
             cout << products[i].code << "\t" << products[i].dimension << "\t\t" << purchased_qty[i]
                  << "\t" << products[i].unit_price << "\t\t" << subtotal
                  << "\t\t" << tps << "\t" << tvq << "\t" << total << endl;
         }
     }
  
+    //Display outputs
     cout << "----------------------------------------------------------------------------------------------------" << endl;
     cout << "----------------------------------------------------------------------------------------------------" << endl;
     cout << endl << "NOMBRE DE PRODUITS ACHETES \t:\t" << total_products << "\titems" << endl;
@@ -430,11 +461,13 @@ void replenish_Stock_Inventory(Product products[], int size) {
 
         if (products[i].code == code) {
 
+            //Display outputs
             one_Information(products[i]);
 
 
             do {
 
+                //Invite, read and save inputs
                 cout << "How many new items to ADD to the stock?:" << endl;
                 cin >> quantity;
 
@@ -448,6 +481,8 @@ void replenish_Stock_Inventory(Product products[], int size) {
 
             } while (cin.fail() || quantity < 0);
             
+
+            //Calculate
             calculate_Add_ToStock(products[i], quantity);
 
             return;
@@ -521,6 +556,7 @@ void calculate_User_Purchase(Product products[], int size, int purchased_qty[]) 
             do {
                 if (code == 50) {
 
+                    //Invite, read and save inputs
                     do {
 
                         cout << endl << "Enter the QUANTITY you bought or 0 if this product is out of stock:" << endl;
@@ -550,6 +586,7 @@ void calculate_User_Purchase(Product products[], int size, int purchased_qty[]) 
                         do {
                             if (quantity > products[0].current_stock) {
 
+                                //Display outputs
                                 cout << "ERROR! You have only " << products[0].current_stock << " in stock. You cannot buy " << quantity << " unit of this product now." << endl << endl;
                                 all_good = false;
 
@@ -582,6 +619,7 @@ void calculate_User_Purchase(Product products[], int size, int purchased_qty[]) 
                 
                 else if (code == 100) {
 
+                    //Invite, read and save inputs
                     do {
 
                         cout << endl << "Enter the QUANTITY you bought or 0 if this product is out of stock:" << endl;
@@ -611,6 +649,7 @@ void calculate_User_Purchase(Product products[], int size, int purchased_qty[]) 
                         do {
                             if (quantity > products[1].current_stock) {
 
+                                //Display outputs
                                 cout << "ERROR! You have only " << products[1].current_stock << " in stock. You cannot buy " << quantity << " unit of this product now." << endl << endl;
                                 all_good = false;
 
@@ -625,9 +664,11 @@ void calculate_User_Purchase(Product products[], int size, int purchased_qty[]) 
 
                             else {
 
+                                //Calculate
                                 products[1].current_stock -= quantity;
                                 purchased_qty[1] += quantity;
 
+                                //Display outputs
                                 cout << quantity << " product of dimension " << products[1].dimension << " (" << products[1].code << " ml) have been added to your cart." << endl;
                                 all_good = false;
                                 break;
@@ -641,6 +682,7 @@ void calculate_User_Purchase(Product products[], int size, int purchased_qty[]) 
                 
                 else if (code == 250) {
 
+                    //Invite, read and save inputs
                     do {
 
                         cout << endl << "Enter the QUANTITY you bought or 0 if this product is out of stock:" << endl;
@@ -670,6 +712,7 @@ void calculate_User_Purchase(Product products[], int size, int purchased_qty[]) 
                         do {
                             if (quantity > products[2].current_stock) {
 
+                                //Display outputs
                                 cout << "ERROR! You have only " << products[2].current_stock << " in stock. You cannot buy " << quantity << " unit of this product now." << endl << endl;
                                 all_good = false;
 
@@ -684,9 +727,11 @@ void calculate_User_Purchase(Product products[], int size, int purchased_qty[]) 
 
                             else {
 
+                                //Calculate
                                 products[2].current_stock -= quantity;
                                 purchased_qty[2] += quantity;
 
+                                //Display outputs
                                 cout << quantity << " product of dimension " << products[2].dimension << " (" << products[2].code << " ml) have been added to your cart." << endl;
                                 all_good = false;
                                 break;
@@ -700,6 +745,7 @@ void calculate_User_Purchase(Product products[], int size, int purchased_qty[]) 
                 
                 else if (code == 500) {
 
+                    //Invite, read and save inputs
                     do {
 
                         cout << endl << "Enter the QUANTITY you bought or 0 if this product is out of stock:" << endl;
@@ -729,6 +775,7 @@ void calculate_User_Purchase(Product products[], int size, int purchased_qty[]) 
                         do {
                             if (quantity > products[3].current_stock) {
 
+                                //Display outputs
                                 cout << "ERROR! You have only " << products[3].current_stock << " in stock. You cannot buy " << quantity << " unit of this product now." << endl << endl;
                                 all_good = false;
 
@@ -743,9 +790,11 @@ void calculate_User_Purchase(Product products[], int size, int purchased_qty[]) 
 
                             else {
 
+                                //Calculate
                                 products[3].current_stock -= quantity;
                                 purchased_qty[3] += quantity;
 
+                                //Display outputs
                                 cout << quantity << " product of dimension " << products[3].dimension << " (" << products[3].code << " ml) have been added to your cart." << endl;
                                 all_good = false;
                                 break;
@@ -770,9 +819,11 @@ void calculate_User_Purchase(Product products[], int size, int purchased_qty[]) 
 
 void write_Stock_Inventory(const Product products[], int size) {
 
+    //Declare variables and constants and initialize
     int code;
     bool code_verif = false;
 
+    //Invite, read and save inputs
     do {
 
         cout << "Enter the CODE of the product (50, 100, 250, or 500) to display its details: " << endl;
@@ -783,17 +834,23 @@ void write_Stock_Inventory(const Product products[], int size) {
 
             cin.clear();
             cin.ignore(numeric_limits<streamsize>::max(), '\n');
+            
+            //Display outputs
             cout << "The product code entered is not valid." << endl << endl;
             code_verif = false;
 
         }
 
         for (int i = 0; i < size; i++) {
+
             if (products[i].code == code) {
+
                 cout << endl;
                 code_verif = true;
+                //Display outputs
                 information(products[i]);
                 return;
+
             }
         }
 
